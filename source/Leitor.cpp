92d4ccfbe52696fd86931a56663e7bbfdcac5a3a
pedropaiva186 <pedrohenriq1860@gmail.com>
2025-09-17 21:07:26 -0300

    Adicionando a função de leitura de arquivo e implementação inicial do criador do autômato

--- source/Leitor.cpp
+++ source/Leitor.cpp
@@ -1,1 +1,33 @@
 #include "Leitor.h"
+
+void Leitor::lerJson(std::string path)
+{
+    std::ifstream f(path);
+
+    if(!f) // Caso não seja possível abrir o arquivo
+    {
+        std::cout << "Não foi possível abrir o arquivo!" << std::endl;
+        return;
+    }
+
+    f >> file;
+    std::cout << "Sucesso ao abrir o arquivo!" << std::endl;
+}
+
+Automato& Leitor::criarAutomato()
+{
+    if(file == NULL) // Caso não tenha lido o arquivo, por algum erro ou engano
+    {
+        std::cout << "O arquivo ainda não foi lido! Falha ao criar o autômato!" << std::endl;
+        return;
+    }
+
+    // Recolhendo os dados dispostos no arquivo de forma fácil
+    std::vector<std::string> estados = file["estados"];
+    std::vector<std::string> alfabeto_entrada = file["alfabeto_entrada"]; 
+    std::vector<std::string> alfabeto_pilha = file["alfabeto_pilha"]; 
+    std::string estado_inicial = file["estado_inicial"];
+    std::vector<std::string> estados_finais = file["estados_finais"];
+
+    // Recolhendo as informações das transições
+}
--- source/Leitor.h
+++ source/Leitor.h
@@ -15,8 +15,17 @@ using json = nlohmann::json;
 class Leitor
 {
     public:
+        // Construtor da classe
+        Leitor()
+        {
+            file = NULL;
+        }
+
+        // Método responsável por ler o arquivo e armazená-lo no campo designado
         void lerJson(std::string path);
-        Automato criarAutomato();
+
+        // Cria o autômato a partir das informações já retiradas do arquivo pela função "lerJson"
+        Automato& criarAutomato();
 
     private:
         json file;
